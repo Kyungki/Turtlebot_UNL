b1a280c871bfeb614e7437df60ba9f78b0c4e453
Cosmo Borsky <cosmo@dabitindustries.com>
2017-04-23 18:31:46 -0400

    fix remove_call function

--- Setup/automated/automate.sh
+++ Setup/automated/automate.sh
@@ -12,20 +12,19 @@ sed -i '1s!^!export DABIT_DIR='"$DABIT_DIR"'\n!' ~/.dabit_aliases
 
 . ~/.dabit_aliases
 
-function remove_call(){
-  echo "sed 's/$1/#&/g' $2"
-  sed 's/$1/#&/g' $2
+remove_call(){
+  sed -i 's/'"$1"'/#&/g' "$2"
 }
 
 sed -i -e '/-f ~\/.dabit_aliases/,+15d' ~/.bashrc
-sed -i "s/^source \/opt\/ros\/kinetic\/setup.bash/#&/g" ~/.bashrc
-sed -i "s/^export TURTLEBOT_BASE=/#&/g" ~/.bashrc
-sed -i "s/^export TURTLEBOT_3D_SENSOR=/#&/g" ~/.bashrc
-sed -i "s/^export TURTLEBOT_STACK=/#&/g" ~/.bashrc
-sed -i "s/^export ROS_MASTER_URI=/#&/g" ~/.bashrc
-sed -i "s/^export ROS_IP=/#&/g" ~/.bashrc
-sed -i "s/^export ROS_HOSTNAME=/#&/g" ~/.bashrc
-sed -i "s/^export ROS_HOME=/#&/g" ~/.bashrc
+remove_call "source \/opt\/ros\/kinetic\/setup.bash" ~/.bashrc
+remove_call "export TURTLEBOT_BASE=" ~/.bashrc
+remove_call "export TURTLEBOT_3D_SENSOR=" ~/.bashrc
+remove_call "export TURTLEBOT_STACK=" ~/.bashrc
+remove_call "export ROS_MASTER_URI=" ~/.bashrc
+remove_call "export ROS_IP=" ~/.bashrc
+remove_call "export ROS_HOSTNAME=" ~/.bashrc
+remove_call "export ROS_HOME=" ~/.bashrc
 
 echo "
 if [ -f ~/.dabit_aliases ]; then
